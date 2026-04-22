6b3f0093465143df0284ca92b2c1d2271af115d6
Tassio Tristao <tassio.tristao05@gmail.com>
2026-04-21 23:16:31 -0300

    (feat): pratica 1 - parte 2

---
+++ app/src/main/java/com/ifpe/tanajura/LoginActivity.kt
@@ -0,0 +1,109 @@
+package com.ifpe.tanajura
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val activity = LocalActivity.current as Activity
+
+    Column(
+        modifier = modifier.fillMaxWidth(fraction = 0.9f),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+
+        Spacer(modifier = modifier.size(12.dp))
+
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = modifier,
+            onValueChange = { email = it }
+        )
+
+        Spacer(modifier = modifier.size(12.dp))
+
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = modifier,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = modifier.size(12.dp))
+
+        Row(
+            modifier = modifier.padding(12.dp).fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show() },
+                enabled = email.isNotEmpty() && password.isNotEmpty()
+            ) {
+                Text("Login")
+            }
+
+            Spacer(modifier = modifier.size(12.dp))
+
+            Button(
+                onClick = { email = ""; password = "" }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}

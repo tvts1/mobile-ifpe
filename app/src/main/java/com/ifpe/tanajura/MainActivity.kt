b1abe98c95fd687d5644174fea135435116d42d3
Tassio Tristao <tassio.tristao05@gmail.com>
2026-05-07 00:49:15 -0300

    (feat): adiciona listpage e mappage

--- app/src/main/java/com/ifpe/tanajura/MainActivity.kt
+++ app/src/main/java/com/ifpe/tanajura/MainActivity.kt
@@ -4,21 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ifpe.tanajura.ui.theme.TanajuraTheme
-import android.app.Activity
-import androidx.activity.compose.LocalActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,27 +22,6 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalActivity.current as Activity
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        // Mensagem de boas-vindas
-        Text(
-            text = "Bem-vindo à Tanajura!",
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        Button ({ activity.finish()}) {
-            Text(text = "Sair")
         }
     }
 }

297acd7d6c93d480c03efcc8a06d2a56e5116d26
Tassio Tristao <tassio.tristao05@gmail.com>
2026-05-07 01:49:34 -0300

    (feat): adiciona botao para criar cidade

--- app/src/main/java/com/ifpe/tanajura/MainActivity.kt
+++ app/src/main/java/com/ifpe/tanajura/MainActivity.kt
@@ -17,6 +17,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.ifpe.tanajura.ui.nav.BottomNavBar
@@ -34,8 +38,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var showDialog by remember { mutableStateOf(false) }
             val navController = rememberNavController()
             TanajuraTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    })
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -60,7 +71,7 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController, items)
                     },
                     floatingActionButton = {
-                        FloatingActionButton (onClick = { }) {
+                        FloatingActionButton (onClick = { showDialog = true }) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }

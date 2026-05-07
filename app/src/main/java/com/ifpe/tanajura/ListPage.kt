f6bc9376bee04347004ed884dc8ef82390fd3e16
Tassio Tristao <tassio.tristao05@gmail.com>
2026-05-07 01:28:23 -0300

    (feat): adiciona a lista de cidades

--- app/src/main/java/com/ifpe/tanajura/ListPage.kt
+++ app/src/main/java/com/ifpe/tanajura/ListPage.kt
@@ -1,33 +1,91 @@
 package com.ifpe.tanajura
 
-import androidx.compose.foundation.background
+import android.app.Activity
+import android.widget.Toast
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ifpe.tanajura.model.City
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun ListPage(modifier: Modifier = Modifier) {
-    Column(
+    val cityList = remember { getCities().toMutableStateList() }
+
+    val activity = LocalActivity.current as Activity
+
+    LazyColumn (
         modifier = modifier
             .fillMaxSize()
-            .background(Color.Magenta)
-            .wrapContentSize(Alignment.Center)
+            .padding(8.dp)
     ) {
-        Text(
-            text = "Favoritas",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        items(
+            items = cityList,
+            key = { it.name }
+        ) { city ->
+            CityItem(
+                city = city,
+                onClose = {
+                    Toast.makeText(activity, "Removendo ${city.name}", Toast.LENGTH_SHORT).show()
+                },
+                onClick = {
+                    Toast.makeText(activity, "Você clicou em ${city.name}", Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
     }
+}
+
+@Composable
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row (
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
+        )
+        Spacer(modifier = Modifier.size(12.dp))
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather?:"Carregando clima...",
+                fontSize = 16.sp)
+        }
+        IconButton (onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+private fun getCities() = List(20) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
 }
---
+++ app/src/main/java/com/ifpe/tanajura/model/CIty.kt
@@ -0,0 +1,7 @@
+package com.ifpe.tanajura.model
+
+data class City (
+    val name : String,
+    val weather: String? = null,
+    val location: String? = null
+)

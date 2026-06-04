5f5e2a3f9f6a0e57d3bfa3335de8bc54bb984d18
Tassio Tristao <tassio.tristao05@gmail.com>
2026-06-04 01:39:22 -0300

    pratica 4 - parte 4

--- app/src/main/java/com/ifpe/tanajura/MapPage.kt
+++ app/src/main/java/com/ifpe/tanajura/MapPage.kt
@@ -1,9 +1,13 @@
 package com.ifpe.tanajura
 
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -11,6 +15,9 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.ifpe.tanajura.viewmodel.MainViewModel
+import androidx.compose.runtime.getValue
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 
 @Composable
 fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
@@ -18,9 +25,18 @@ fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
     val caruaru = remember { MarkerState( LatLng(-8.27, -35.98)) }
     val joaopessoa = remember { MarkerState( LatLng(-7.12, -34.84)) }
     val camPosState = rememberCameraPositionState ()
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
     GoogleMap (modifier = Modifier.fillMaxSize(), onMapClick = {
         viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) },
-        cameraPositionState = camPosState) {
+        cameraPositionState = camPosState, properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)) {
         Marker(
             state = recife,
             title = "Recife",
